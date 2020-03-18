4aa454e538cfccbf26cd56521fd392c934534197
Eugene Agafonov <e.a.agafonov@gmail.com>
2020-03-18 16:04:21 -0700

    Update copyright year

--- src/ikonized.cpp
+++ src/ikonized.cpp
@@ -4,7 +4,7 @@
 // Description:
 //
 //
-// Author: Eugene Agafonov <e.a.agafonov@gmail.com>, (C) 2015
+// Author: Eugene Agafonov <e.a.agafonov@gmail.com> (C) 2009-2020
 //
 // Copyright: See COPYING file that comes with this distribution
 //
